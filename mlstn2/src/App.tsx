5b1dde79efac3956194d0296578866df5ac23f9c
LightKazuto <ilhamsidi62@gmail.com>
2024-05-03 19:50:20 +0700

    frist commit

---
+++ mlstn2/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomeComponent from './Pages/home';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <HomeComponent />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
